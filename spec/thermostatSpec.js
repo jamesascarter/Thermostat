c1b202d48d7f73a94c0dae46801558b1e6e19a79
jamesascarter <jamesascarter@gmail.com>
2014-10-22 19:05:31 +0100

    set limits on the temperatures the thermostat can reach

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -2,10 +2,13 @@ describe('Thermostat', function(){
 
 	var thermostat;
 
+	beforeEach(function() {
+		thermostat = new Thermostat
+	});
+
 	describe('by default', function(){
 
 		it('is set to 20 degrees', function(){
-			thermostat = new Thermostat();
 			expect(thermostat.temperature).toEqual(20)
 		});
 
@@ -17,6 +20,28 @@ describe('Thermostat', function(){
 			thermostat.increaseTemperature();
 			expect(thermostat.temperature).toEqual(21);
 		});
+
+		it('can decrease the temp. by one degree', function(){
+			thermostat.decreaseTemperature();
+			expect(thermostat.temperature).toEqual(19);
+		});
+
+		it("should have a maximum temperature of 32", function(){
+			expect(thermostat.maximumTemperature).toEqual(32)
+		});
+
+		it("should not be able to increase more than 32", function(){
+			thermostat.increaseTemperatureBy(12)
+			thermostat.increaseTemperature();
+			expect(thermostat.temperature).toEqual(32)
+		});
+
+		it("should not be able to decrease more than 10 degrees", function(){
+			thermostat.decreaseTemperatureBy(10)
+			thermostat.decreaseTemperature();
+			expect(thermostat.temperature).toEqual(10)
+		});
+
 	});
 
 	describe('custom options', function(){
@@ -25,6 +50,13 @@ describe('Thermostat', function(){
 			expect(thermostat.temperature).toEqual(32);
 
 		});
+
+		it('can decrease the temp by 8', function(){
+			thermostat.decreaseTemperatureBy(8);
+			expect(thermostat.temperature).toEqual(12);
+
+		});
+
 	});
 
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,12 +1,28 @@
 function Thermostat(){
 this.temperature = 20;
 this.isPowerSaverOn = true;
+this.maximumTemperature = 32;
+this.minimumTemperature = 10;
 };
 
 Thermostat.prototype.increaseTemperature = function() {
-	this.temperature = this.temperature + 1
+	if(this.temperature === this.maximumTemperature)
+		this.increaseTemperatureBy(0);
+	else 
+		this.increaseTemperatureBy(1);
 };
 
 Thermostat.prototype.increaseTemperatureBy = function(degrees) {
-	// body...
+	this.temperature += degrees
+};
+
+Thermostat.prototype.decreaseTemperature = function() {
+	if(this.temperature === this.minimumTemperature)
+		this.decreaseTemperatureBy(0);
+	else
+		this.decreaseTemperatureBy(1);
+};
+
+Thermostat.prototype.decreaseTemperatureBy = function(degrees) {
+	this.temperature -= degrees
 };
